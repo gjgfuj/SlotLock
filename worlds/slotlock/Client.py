bce2b82a8e89f38146aa1a4b31696cb78536932a
Sandra Gore <sandra@sandromatic.me>
2026-02-08 13:58:27 +1100

    Fixed password login

--- worlds/slotlock/Client.py
+++ worlds/slotlock/Client.py
@@ -64,8 +64,7 @@ class SlotLockContext(CommonContext):
         CommonContext.__init__(self, server_address, password)
 
     async def server_auth(self, password_requested: bool = False):
-        if password_requested and not self.password:
-            await super(CommonContext, self).server_auth(password_requested)
+        await super().server_auth(password_requested)
         await self.get_username()
         await self.send_connect()
     async def run_checking_hints(self):
