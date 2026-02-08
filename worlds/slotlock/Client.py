3f7eceb8c1559ea8f76ac0ed317890763516b8a3
Sandra Gore <sandra@sandromatic.me>
2026-02-08 13:44:28 +1100

    Fixed issue where logging in with a password doesn't work.

--- worlds/slotlock/Client.py
+++ worlds/slotlock/Client.py
@@ -59,7 +59,7 @@ class SlotLockContext(CommonContext):
 
     async def server_auth(self, password_requested: bool = False):
         if password_requested and not self.password:
-            await super(TextContext, self).server_auth(password_requested)
+            await super(CommonContext, self).server_auth(password_requested)
         await self.get_username()
         await self.send_connect()
     async def run_checking_hints(self):
